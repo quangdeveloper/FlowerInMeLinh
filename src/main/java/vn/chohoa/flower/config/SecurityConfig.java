253435bb553091c8de40b8d88496030c9f6afa1d
quang nguyen <muadongquehuongtoi@gmail.com>
2020-05-11 13:57:30 +0700

    second 11-05-2020

--- src/main/java/vn/chohoa/flower/config/SecurityConfig.java
+++ src/main/java/vn/chohoa/flower/config/SecurityConfig.java
@@ -1,10 +1,108 @@
 package vn.chohoa.flower.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import vn.chohoa.flower.security.CustomUserDetailService;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        prePostEnabled = true,
+        jsr250Enabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
 
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService)
+                .passwordEncoder(passwordEncoder());
+
+    }
+
+    @Value("${app.nonAuthen")
+    private String nonAuthen;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String[] nonAuthens = nonAuthen.split("\\|");
+        http
+                .cors()// để sử dụng tài nguyên chéo từ domain sang domain khác
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+    }
+
+    /**
+     * Cấu hình cho swagger ui
+     *
+     * @param web
+     * @throws Exception
+     */
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "swagger-resoures/**",
+                "configuration/secuirity",
+                "swagger-ui.html",
+                "webjar/**"
+        );
+    }
+
+    /**
+     * Cấu hình truy xuất cheo từ domain khác sang
+     * <p>
+     * addmapping: những URL dc truy xuất
+     * allowOrigin: đc phép truy xuất từ domain nào
+     * allowMethod: Những thương thúc đc phép truy xuất
+     * maxAge: thời gian truy xuất tối đa
+     *
+     * @param registry
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "DELETE", "PUT")
+                .maxAge(3600);
+    }
 }
