3171913c711d227a9b647ee51662d58595bcb307
quang <nguyenvanquangk11@gmail.com>
2020-05-14 00:17:49 +0700

    12:14 AM 14-05-2020

--- src/main/java/vn/chohoa/flower/dto/MojiDTO.java
+++ src/main/java/vn/chohoa/flower/dto/MojiDTO.java
@@ -1,4 +1,27 @@
 package vn.chohoa.flower.dto;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MojiDTO {
+
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    private Long groupId;
+
+    private String groupName;
+
+    private String content;
+
 }
---
+++ src/main/java/vn/chohoa/flower/mapper/MojiMapper.java
@@ -0,0 +1,5 @@
+package vn.chohoa.flower.mapper;
+
+
+public interface MojiMapper {
+}
